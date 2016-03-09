a940c07ddfb011656cc51a52724a141563f3e16d
ringo156 <blackapple262@gmail.com>
2016-03-10 01:42:17 +0900

    メイン基板のプロジェクトを追加

---
+++ intelligent_robo/mainpcb_test.cydsn/main.c
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    Motor_PWM_a_Start();
+    Motor_PWM_b_Start();
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
