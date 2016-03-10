ee9be3ace530589ebda2646f3374cc22c5080b3c
ringo156 <blackapple262@gmail.com>
2016-03-10 20:58:01 +0900

    PS2コントローラーとI2C液晶の処理を追加

--- intelligent_robo/mainpcb_test.cydsn/main.c
+++ intelligent_robo/mainpcb_test.cydsn/main.c
@@ -10,19 +10,82 @@
  * ========================================
 */
 #include <project.h>
+#include <stdio.h>
+#include "PS2_Controller.h"
+
+void I2C_Color_init(void);
+void I2C_LCD_Position(uint8 row, uint8 column);
+void I2C_LCD_Init(void);
 
 int main()
 {
+    char value[20];
+    PS2Controller psData;
     CyGlobalIntEnable; /* Enable global interrupts. */
-
+    CyDelay(500);
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     Motor_PWM_a_Start();
     Motor_PWM_b_Start();
-
+    PS2_Start();
+    I2C_1_Start();
+    I2C_LCD_1_Start();
+    I2C_LCD_Init();
+    Debug_LED_Write(1);
+    CyDelay(1000);
+    sprintf(value, "PSoC5 Start");
+    I2C_LCD_Position(0u,0u);
+    I2C_LCD_1_PrintString(value);
     for(;;)
     {
         /* Place your application code here. */
+        psData = PS2_Controller_get();
+        if(psData.CIRCLE)
+        {
+            Debug_LED_Write(0);
+        }
+
+        //Debug_LED_Write(1);
     }
 }
 
+
+void I2C_Color_init(void)
+{
+    unsigned char txCBuf[2] = { 0x0, 0x89 };
+    //I2C_1_Start();
+    
+    I2C_1_MasterWriteBuf(0x2A,(uint8 *)&txCBuf,2,I2C_1_MODE_COMPLETE_XFER);
+    while(0u==(I2C_1_MasterStatus() & I2C_1_MSTAT_WR_CMPLT));
+    I2C_1_MasterClearStatus();
+    txCBuf[1] = 0x09;
+    I2C_1_MasterWriteBuf(0x2A,(uint8 *)&txCBuf,2,I2C_1_MODE_COMPLETE_XFER);
+    while(0u==(I2C_1_MasterStatus() & I2C_1_MSTAT_WR_CMPLT));
+    I2C_1_MasterClearStatus();
+}
+
+void I2C_LCD_Position(uint8 row, uint8 column) {
+ 
+  if(row == 0){
+    I2C_LCD_1_SetDDRAM(0x80u + column);
+  }else{
+    I2C_LCD_1_SetDDRAM(0xC0u + column);
+  }
+}
+
+void I2C_LCD_Init(void) {
+    CyDelay(40u);     //40mS 待機
+    I2C_LCD_1_FunctionSetNormal();
+    I2C_LCD_1_ReturnHome();
+    I2C_LCD_1_FunctionSetExtended();
+    I2C_LCD_1_InternalOscFrequency();
+    I2C_LCD_1_ContrastSet();
+    I2C_LCD_1_PwrIconContrast();
+    I2C_LCD_1_FollowerCtrl();
+    CyDelay(200u);     //200mS 待機
+    I2C_LCD_1_FunctionSetNormal();
+    I2C_LCD_1_DisplayOn();
+    I2C_LCD_1_Clear();
+    I2C_LCD_1_EntryModeSet();
+    CyDelay(1);    //I2C_LCD_1_WriteControl(0x0fu); //Disp:On Cursor:On Position:On
+}
 /* [] END OF FILE */
